08b3d0e7bd337cb86bae2ba406b5e316b45dce0c
Ivan <vanyok.korobka@ukr.net>
2021-09-02 23:25:25 +0300

    lessons-39 - add added the ability to switch information between food styles

--- js/script.js
+++ js/script.js
@@ -2,7 +2,50 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
     const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent')  
+          tabsContent = document.querySelectorAll('.tabcontent'), // содержит блоки информации табов  
+          tabsParent = document.querySelector('.tabheader__items');
+          
+    function hideTabContent() { // функция, которая скрывает табы (блоки инфы)
+        
+        tabsContent.forEach(item => { // скрывает информацию раздела (таба)
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => { // удаляем активный класс
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+    
+    function showTabContent(i = 0) { // функция, которая заново показывает информацию таба (раздела)
+        tabsContent[i].style.display = 'block'; 
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    /*
+    ниже делаем делегирование события, которое по 
+    условию смотрит есть ли полученный объект данных и 
+    имеет ли он класс tabheader__item (неактивного раздела),
+    если да, то он сравнивает
+    полученный объект с объектами из tabs (то есть, 
+    сравнивает название роздела, на которое нажал юзер, с 
+    названиями разделов tabs) Если есть совпадение, то запускаются
+    функции скрытия разделов и отображения необходимого раздела)
+    */
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
 
 
 });
