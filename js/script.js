504eaef81ecaf0a62745afd897e9221da5d11251
Ivan <vanyok.korobka@ukr.net>
2021-09-02 22:13:38 +0300

    start this project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent')  
+
+
+});
