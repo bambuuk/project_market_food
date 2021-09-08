b495ab1661970e654734113f194f4548f8942d51
Ivan <vanyok.korobka@ukr.net>
2021-09-09 00:37:19 +0300

    48 lessons - upd our function with suport classes method

--- js/script.js
+++ js/script.js
@@ -158,13 +158,87 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    const modalTimerId = setTimeout(openModal, 5000);
+    // const modalTimerId = setTimeout(openModal, 5000);
 
     window.addEventListener('scroll', () => {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
             openModal();
             }
     });
+
+    // Используем классы для карточек
+
+    /**Создаём класс, в котором мы сохраняем инфу для построения карточек меню */
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        /**Функция, которая конвектирует доллары в грн */
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        /**Метод, который отображает ел на странице */
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+            <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard( // создаём карточку 1
+        "img/tabs/vegy.jpg",
+        "vegy",
+        `Меню "Фитнес"`,
+        `Меню "Фитнес" - это новый подход к приготовлению блюд: 
+        больше свежих овощей и фруктов. Продукт активных и здоровых
+         людей. Это абсолютно новый продукт с оптимальной ценой
+          и высоким качеством!`,
+          9,
+          '.menu .container'
+    ).render();
+
+    new MenuCard( // создаём карточку 2
+        "img/tabs/elite.jpg",
+        "elite",
+        `Меню "Премиум"`,
+        `В меню “Премиум” мы используем не только красивый дизайн упаковки, 
+        но и качественное исполнение блюд. Красная рыба, морепродукты, 
+        фрукты - ресторанное меню без похода в ресторан!`,
+          21,
+          '.menu .container'
+    ).render();
+
+    new MenuCard( // создаём карточку 3
+        "img/tabs/post.jpg",
+        "post",
+        `Меню "Постное"`,
+        `В меню “Премиум” мы используем не только красивый дизайн упаковки, 
+        но и качественное исполнение блюд. Красная рыба, морепродукты, 
+        фрукты - ресторанное меню без похода в ресторан!`,
+          16,
+          '.menu .container'
+    ).render();
+
 
 
 });
